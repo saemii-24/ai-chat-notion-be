9a43b2b41396f27cd56d779d239fe30900c3e99f
saemii-24 <hims2419@gmail.com>
2026-03-19 22:01:47 +0900

    feat: postgresql로 이전

--- app/db/db.py
+++ app/db/db.py
@@ -1,12 +1,13 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./test.db"
+load_dotenv()
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
